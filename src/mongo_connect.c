93c220dfc57673c92d3952e202eba82e6192aeab
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-05 13:31:17 +0200

    Add proof of concept for automatic garbage collection

--- src/mongo_connect.c
+++ src/mongo_connect.c
@@ -2,6 +2,9 @@
 #include <bson.h>
 #include <mongoc.h>
 
+SEXP R_mongo_connect(SEXP uri_string, SEXP db, SEXP collection);
+void r_mongo_clean (SEXP ptr);
+
 SEXP R_mongo_connect(SEXP uri_string, SEXP db, SEXP collection) {
     mongoc_client_t *client;
     mongoc_collection_t *col;
@@ -12,5 +15,18 @@ SEXP R_mongo_connect(SEXP uri_string, SEXP db, SEXP collection) {
     }
 
     col = mongoc_client_get_collection (client, CHAR(asChar(db)), CHAR(asChar(collection)));
-    return R_MakeExternalPtr(col, R_NilValue, R_NilValue);
+    SEXP ptr = PROTECT(R_MakeExternalPtr(col, R_NilValue, R_NilValue));
+
+    //clean up on garbage collection
+    R_RegisterCFinalizerEx(ptr, r_mongo_clean, 1);
+    UNPROTECT(1);
+    return ptr;
 }
+
+void r_mongo_clean(SEXP ptr){
+  Rprintf("DEBUG: Destorying collection.\n");
+  if(!R_ExternalPtrAddr(ptr)) return;
+  mongoc_collection_destroy(R_ExternalPtrAddr(ptr));
+  R_ClearExternalPtr(ptr);
+}
+
