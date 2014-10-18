54456b3d688531297a3fc20fa339ebd2023a3884
Jeroen Ooms <jeroenooms@gmail.com>
2014-10-18 11:44:35 -0700

    Fix

--- src/bson.c
+++ src/bson.c
@@ -7,20 +7,19 @@ SEXP R_json_to_bson(SEXP json);
 void fin_bson(SEXP ptr);
 
 SEXP R_json_to_bson(SEXP json){
-  bson_t b;
+  bson_t *b;
   bson_error_t err;
-  if(!bson_init_from_json(&b, translateCharUTF8(asChar(json)), -1, &err))
+
+  b = bson_new_from_json ((uint8_t*)translateCharUTF8(asChar(json)), -1, &err);
+
+  if(!b)
     error(err.message);
 
   //destroy on gc
-  SEXP ptr = PROTECT(R_MakeExternalPtr(&b, R_NilValue, R_NilValue));
+  SEXP ptr = PROTECT(R_MakeExternalPtr(b, R_NilValue, R_NilValue));
   R_RegisterCFinalizerEx(ptr, fin_bson, 1);
   setAttrib(ptr, R_ClassSymbol, mkString("bson"));
   UNPROTECT(1);
-
-  //debug:
-  //bson_t *doc = R_ExternalPtrAddr(ptr);
-  //Rprintf(bson_as_json (doc, NULL));
   return ptr;
 }
 
@@ -28,9 +27,6 @@ SEXP R_bson_to_json(SEXP ptr){
   bson_t *b = R_ExternalPtrAddr(ptr);
   if(!b)
     error("BSON document is null.");
-
-  //this gives segfault
-  //libbson seems to free up the bson object even when we do not call bson_destroy.
   return mkStringUTF8(bson_as_json (b, NULL));
 }
 
