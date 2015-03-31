06664540c81538d3ad72285169cb3d8f12952569
Jeroen Ooms <jeroenooms@gmail.com>
2015-03-31 14:22:53 -0700

    Add comment

--- test/ssl.R
+++ test/ssl.R
@@ -1,3 +1,6 @@
+# Default brew build does not support SSL
+# You need: brew install mongodb --with-openssl
+
 library(mongolite)
 m <- mongo_connect("mongodb://localhost/?ssl=true", collection = "ssltest")
 mongo_stream_out(mtcars, m)
