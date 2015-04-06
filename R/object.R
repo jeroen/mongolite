8af9153ef5978660590ff774329e9ed1e8d4d183
Jeroen Ooms <jeroenooms@gmail.com>
2015-04-05 17:55:16 -0700

    Documentation

--- R/object.R
+++ R/object.R
@@ -1,33 +1,46 @@
 #' MongoDB client
 #'
-#' Initiate a new MongoDB collection.
+#' Connect to a MongoDB collection.
 #'
 #' @export
 #' @param url address of the mongodb server
 #' @param db name of database
 #' @param collection name of collection
-#' @examples \dontrun{dplyr example
+#' @examples \dontrun{
+#' # dplyr example
 #' library(nycflights13)
 #'
 #' # Insert some data
-#' con <- mongo(collection = "myflights")
-#' if(con$count() > 0) con$drop()
-#' con$insert(flights)
+#' m <- mongo(collection = "myflights")
+#' m$drop()
+#' m$insert(flights)
 #'
-#' # Basic operations
-#' con$count('{"month":1, "day":1}')
-#' mydata <- con$find(query = '{"month":1, "day":1}')
+#' # Basic queries
+#' m$count('{"month":1, "day":1}')
+#' jan1 <- m$find('{"month":1, "day":1}')
+#'
+#' # Sorting
+#' jan1 <- m$find('{"$query":{"month":1,"day":1}, "$orderby":{"distance":-1}}')
+#' head(jan1)
+#'
+#' # Select columns
+#' jan1 <- m$find('{"month":1,"day":1}', fields = '{"_id":0, "distance":1, "carrier":1}')
+#'
+#' # Tabulate
+#' aggregates <- m$aggregate('[{"$group":{"_id":"$carrier", "count": {"$sum":1}, "average":{"$avg":"$distance"}}}]')
 #' }
 #' @section Methods:
 #' \describe{
-#'   \item{\code{count(query = '{}')}}{Count the number of records for a given query. If no query is specified, counts all records.}
-#'   \item{\code{drop()}}{Delete entire collection and all data.}
-#'   \item{\code{find(query = '{}', fields = '{"_id" : 0}', skip = 0, limit = 0, handler = NULL, pagesize = 1000, verbose = TRUE)}}{Retrieve 'fields' from all records matching a query. If handler = NULL, data will returned as a single data frame.}
-#'   \item{\code{index(add = NULL, remove = NULL)}}{find, add or remove indexes from the collection. Always returns a data frame with current indexes.}
+#'   \item{\code{aggregate(pipeline = '{}', handler = NULL, pagesize = 1000, verbose = TRUE)}}{Execute a pipeline using the Mongo aggregation framework.}
+#'   \item{\code{count(query = '{}')}}{Count the number of records matching a given \code{query}. Default counts all records in collection.}
+#'   \item{\code{drop()}}{Delete entire collection with all data and metadata.}
+#'   \item{\code{find(query = '{}', fields = '{"_id" : 0}', skip = 0, limit = 0, handler = NULL, pagesize = 1000, verbose = TRUE)}}{Retrieve \code{fields} from records matching \code{query}. Default \code{handler} will return all data as a single dataframe.}
+#'   \item{\code{index(add = NULL, remove = NULL)}}{List, add, or remove indexes from the collection. Returns a dataframe with current indexes.}
 #'   \item{\code{info()}}{Returns collection statistics and server info (if available).}
-#'   \item{\code{insert(data, pagesize = 1000, verbose = TRUE)}}{Insert a data frame into the collection.}
-#'   \item{\code{remove(query = "{}", multiple = TRUE)}}{Remove record(s) from the collection.}
-#'   \item{\code{rename(name, db = "test")}}{Change the name or database of a collection. Changing name is cheap, but changing database is expensive.}
+#'   \item{\code{insert(data, pagesize = 1000, verbose = TRUE)}}{Insert a dataframe into the collection.}
+#'   \item{\code{remove(query = "{}", multiple = FALSE)}}{Remove record(s) matching \code{query} from the collection.}
+#'   \item{\code{rename(name, db = "test")}}{Change the name or database of a collection. Changing name is cheap, changing database is expensive.}
+#'   \item{\code{update(query, update = '{"$set":{}}', upsert = FALSE, multiple = FALSE)}}{Replace or modify matching record(s) with value of the \code{update} argument.}
 #' }
 mongo <- function(collection = "test",  db = "test", url = "mongodb://localhost"){
   con <- mongo_collection_new(url, db, collection)
