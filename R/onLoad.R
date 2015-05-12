af7d1217d3c25102c0c054c2500c69ccf6f5aff1
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-12 16:34:52 -0700

    Move onLoad to plain C.

--- R/onLoad.R
+++
@@ -1,9 +0,0 @@
-#' @useDynLib mongolite R_mongo_init
-.onLoad <- function(libname, pkgname){
-  .Call(R_mongo_init)
-}
-
-#' @useDynLib mongolite R_mongo_cleanup
-.onUnload <- function(libpath){
-  .Call(R_mongo_cleanup)
-}
--- src/onload.c
+++ src/onload.c
@@ -1,33 +1,32 @@
+#include <R_ext/Rdynload.h>
 #include <mongolite.h>
-
-SEXP R_mongo_init() {
-  static mongoc_log_func_t logfun = mongolite_log_handler;
-  mongoc_init();
-  mongoc_log_set_handler(logfun, NULL);
-  return R_NilValue;
-}
-
-SEXP R_mongo_cleanup() {
-  mongoc_cleanup();
-  return R_NilValue;
-}
 
 void mongolite_log_handler (mongoc_log_level_t log_level, const char *log_domain, const char *message, void *user_data) {
   switch (log_level) {
-    case MONGOC_LOG_LEVEL_ERROR:
-      stop(message);
-      break;
-    case MONGOC_LOG_LEVEL_CRITICAL:
-    case MONGOC_LOG_LEVEL_WARNING:
-      Rf_warningcall_immediate(R_NilValue, message);
-      break;
-    case MONGOC_LOG_LEVEL_MESSAGE:
-    case MONGOC_LOG_LEVEL_INFO:
-      Rprintf("Mongo Message: %s\n", message);
-      break;
-    case MONGOC_LOG_LEVEL_DEBUG:
-    case MONGOC_LOG_LEVEL_TRACE:
-    default:
-      break;
+  case MONGOC_LOG_LEVEL_ERROR:
+    stop(message);
+    break;
+  case MONGOC_LOG_LEVEL_CRITICAL:
+  case MONGOC_LOG_LEVEL_WARNING:
+    Rf_warningcall_immediate(R_NilValue, message);
+    break;
+  case MONGOC_LOG_LEVEL_MESSAGE:
+  case MONGOC_LOG_LEVEL_INFO:
+    Rprintf("Mongo Message: %s\n", message);
+    break;
+  case MONGOC_LOG_LEVEL_DEBUG:
+  case MONGOC_LOG_LEVEL_TRACE:
+  default:
+    break;
   }
+}
+
+void R_init_mongolite(DllInfo *info) {
+  static mongoc_log_func_t logfun = mongolite_log_handler;
+  mongoc_init();
+  mongoc_log_set_handler(logfun, NULL);
+}
+
+void R_unload_mongolite(DllInfo *info) {
+  mongoc_cleanup();
 }
