7a766c32fc4e249d16364b13d65d0be02fc994d8
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-11 17:15:32 -0700

    Update mongo-c-driver to 1.1.5-dev

--- src/bson/bson-stdint.h
+++ src/bson/bson-stdint.h
@@ -2,7 +2,7 @@
 #define _SRC_BSON_BSON_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H " "
-/* generated using a gnu compiler version Apple LLVM version 6.0 (clang-600.0.57) (based on LLVM 3.5svn) Target: x86_64-apple-darwin14.1.0 Thread model: posix */
+/* generated using a gnu compiler version Apple LLVM version 6.1.0 (clang-602.0.49) (based on LLVM 3.6.0svn) Target: x86_64-apple-darwin14.3.0 Thread model: posix */
 
 #include <stdint.h>
 
