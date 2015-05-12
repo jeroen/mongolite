9cd41dc544f54855350e3041a87c1ce71e0eccdf
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-12 15:54:29 -0700

    This is no longer needed.

--- src/apple.h
+++ src/apple.h
@@ -7,10 +7,3 @@
 #endif
 #endif
 
-/* workaround for non ANSI-C BSON_MAX and BSON_MIN macros */
-/* not needed, CRAN seems to be nice today
-#ifdef __GNUC__
-#define MIN(a, b) (((a) < (b)) ? (a) : (b))
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#endif
-*/
