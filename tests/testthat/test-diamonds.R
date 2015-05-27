cb0aafcac38160fd8b011fdc6182305ef6b7a4bc
Jeroen Ooms <jeroenooms@gmail.com>
2015-05-28 01:06:12 +0200

    Start with tests

---
+++ tests/testthat/test-diamonds.R
@@ -0,0 +1,28 @@
+context("diamonds")
+data(diamonds, package = "ggplot2")
+
+m <- mongo("test_diamonds", verbose = FALSE)
+if(m$count()) m$drop()
+
+test_that("insert data", {
+  m$insert(diamonds)
+  expect_equal(m$count(), nrow(diamonds))
+})
+
+test_that("find query", {
+  out1 <- m$find('{"cut" : "Premium", "price" : { "$lt" : 1000 } }')
+  out2 <- subset(diamonds, cut == "Premium" & price < 1000)
+  expect_equal(nrow(out1), nrow(out2))
+})
+
+test_that("remove data", {
+  m$remove('{"cut" : "Premium", "price" : { "$lt" : 1000 } }', multiple = FALSE)
+  expect_equal(m$count(), nrow(diamonds)-1)
+  m$remove('{"cut" : "Premium", "price" : { "$lt" : 1000 } }', multiple = FALSE)
+  expect_equal(m$count(), nrow(diamonds)-2)
+  m$remove('{"cut" : "Premium", "price" : { "$lt" : 1000 } }', multiple = TRUE)
+  expect_equal(m$count(), nrow(diamonds) - 3200)
+  m$remove('{}', multiple = TRUE)
+  expect_equal(m$count(), 0L)
+  expect_true(m$drop())
+})
---
+++ tests/testthat/test-flights.R
@@ -0,0 +1,18 @@
+context("flights")
+library(nycflights13)
+
+m <- mongo("test_flights", verbose = FALSE)
+if(m$count()) m$drop()
+
+test_that("insert data", {
+  m$insert(flights)
+  expect_equal(m$count(), nrow(flights))
+  jan1 <- m$find('{"month":1, "day":1}')
+  expect_equal(m$count('{"month":1, "day":1}'), nrow(jan1))
+})
+
+test_that("remove data", {
+  m$remove('{}', multiple = TRUE)
+  expect_equal(m$count(), 0L)
+  expect_true(m$drop())
+})
