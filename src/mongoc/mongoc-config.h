e3f6c936dbce003eeee8394f3d8edc63eeb1951a
Jeroen Ooms <jeroenooms@gmail.com>
2015-06-10 01:07:01 +0200

    Disable sasl_client_done for now

--- src/mongoc/mongoc-config.h
+++ src/mongoc/mongoc-config.h
@@ -47,11 +47,8 @@
  * which supersedes sasl_done ().
  */
 
-#if (defined(__sun) && defined(__SVR4))
+/* only available on recent verions of sasl */
 #define MONGOC_HAVE_SASL_CLIENT_DONE 0
-#else
-#define MONGOC_HAVE_SASL_CLIENT_DONE 1
-#endif
 
 #if MONGOC_HAVE_SASL_CLIENT_DONE != 1
 #  undef MONGOC_HAVE_SASL_CLIENT_DONE
