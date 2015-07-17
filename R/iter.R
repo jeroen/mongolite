21bc528c2417b8105ca7ec10501482273001925e
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-17 18:20:10 +0200

    Change iterator interface

---
+++ R/iter.R
@@ -0,0 +1,21 @@
+mongo_iterator <- function(cur){
+  self <- local({
+    one <- function(){
+      mongo_cursor_next_page(cur, size = 1)[[1]]
+    }
+    batch <- function(size = 1000){
+      mongo_cursor_next_page(cur, size = size)
+    }
+    page <- function(size = 1000){
+      as.data.frame(jsonlite:::simplify(mongo_cursor_next_page(cur, size = size)))
+    }
+    environment()
+  })
+  lockEnvironment(self, TRUE)
+  structure(self, class=c("mongo_iter", "jeroen", class(self)))
+}
+
+#' @export
+print.mongo_iter <- function(x, ...){
+  print.jeroen(x, title = paste0("<Mongo iterator>"))
+}
--- R/object.R
+++ R/object.R
@@ -108,9 +108,7 @@ mongo_object <- function(col, client, verbose){
 
     iterate <- function(query = '{}', fields = '{"_id":0}', sort = '{}', skip = 0, limit = 0) {
       cur <- mongo_collection_find(col, query = query, sort = sort, fields = fields, skip = skip, limit = limit)
-      structure(function() {
-        mongo_cursor_next_page(cur, size = 1)
-      }, class = "iterator")
+      mongo_iterator(cur)
     }
 
     export <- function(con = stdout(), bson = FALSE){
