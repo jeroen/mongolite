ae543b1391c27724fca7a2641c04b1218c652869
Jeroen Ooms <jeroenooms@gmail.com>
2016-03-22 12:08:14 +0100

    Add comments

--- src/onload.c
+++ src/onload.c
@@ -3,19 +3,19 @@
 
 void mongolite_log_handler (mongoc_log_level_t log_level, const char *log_domain, const char *message, void *user_data) {
   switch (log_level) {
-  case MONGOC_LOG_LEVEL_ERROR:
+  case MONGOC_LOG_LEVEL_ERROR: //0
     stop(message);
     break;
-  case MONGOC_LOG_LEVEL_CRITICAL:
-  case MONGOC_LOG_LEVEL_WARNING:
+  case MONGOC_LOG_LEVEL_CRITICAL: //1
+  case MONGOC_LOG_LEVEL_WARNING: //2
     Rf_warningcall_immediate(R_NilValue, message);
     break;
-  case MONGOC_LOG_LEVEL_MESSAGE:
-  case MONGOC_LOG_LEVEL_INFO:
+  case MONGOC_LOG_LEVEL_MESSAGE: //3
+  case MONGOC_LOG_LEVEL_INFO: //4
     Rprintf("Mongo Message: %s\n", message);
     break;
-  case MONGOC_LOG_LEVEL_DEBUG:
-  case MONGOC_LOG_LEVEL_TRACE:
+  case MONGOC_LOG_LEVEL_DEBUG: //5
+  case MONGOC_LOG_LEVEL_TRACE: //6
   default:
     break;
   }
