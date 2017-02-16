153506d4fdd35121286c14c14df17fd7f0f36580
Jeroen Ooms <jeroenooms@gmail.com>
2017-02-16 15:39:07 +0100

    Backport UBSAN hack

--- src/mongoc/mongoc-util-private.h
+++ src/mongoc/mongoc-util-private.h
@@ -36,7 +36,7 @@
 
 /* Suppress CWE-252 ("Unchecked return value") warnings for things we can't deal
  * with */
-#if defined(__GNUC__) && __GNUC__ >= 4
+#if defined(__GNUC__) && __GNUC__ >= 99999
 #define _ignore_value(x)       \
    (({                         \
       __typeof__(x) __x = (x); \
