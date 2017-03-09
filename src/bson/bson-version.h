96d4f8a3bbf01a0a59ec5218fe98aa7ada1cb006
Jeroen Ooms <jeroenooms@gmail.com>
2017-03-09 15:04:29 +0100

    Sync with upstream mongo-c-driver v1.6.1

--- src/bson/bson-version.h
+++ src/bson/bson-version.h
@@ -45,7 +45,7 @@
  *
  * BSON micro version component (e.g. 3 if %BSON_VERSION is 1.2.3)
  */
-#define BSON_MICRO_VERSION (0)
+#define BSON_MICRO_VERSION (1)
 
 
 /**
@@ -60,7 +60,7 @@
  *
  * BSON version.
  */
-#define BSON_VERSION (1.6.0)
+#define BSON_VERSION (1.6.1)
 
 
 /**
@@ -69,7 +69,7 @@
  * BSON version, encoded as a string, useful for printing and
  * concatenation.
  */
-#define BSON_VERSION_S "1.6.0"
+#define BSON_VERSION_S "1.6.1"
 
 
 /**
--- src/mongoc/mongoc-version.h
+++ src/mongoc/mongoc-version.h
@@ -45,7 +45,7 @@
  *
  * MONGOC micro version component (e.g. 3 if %MONGOC_VERSION is 1.2.3)
  */
-#define MONGOC_MICRO_VERSION (0)
+#define MONGOC_MICRO_VERSION (1)
 
 
 /**
@@ -61,7 +61,7 @@
  *
  * MONGOC version.
  */
-#define MONGOC_VERSION (1.6.0)
+#define MONGOC_VERSION (1.6.1)
 
 
 /**
@@ -70,7 +70,7 @@
  * MONGOC version, encoded as a string, useful for printing and
  * concatenation.
  */
-#define MONGOC_VERSION_S "1.6.0"
+#define MONGOC_VERSION_S "1.6.1"
 
 
 /**
