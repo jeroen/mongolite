a3f6b139225c80334f97c1c712800730b25dc88d
Arkadiusz Gladki <arkadiusz.gladki@contractors.roche.com>
2018-11-27 17:48:40 +0100

    Update unit tests (for create unique index)

--- tests/testthat/test-diamonds.R
+++ tests/testthat/test-diamonds.R
@@ -3,6 +3,7 @@ data(diamonds, package = "ggplot2")
 
 m <- mongo("test_diamonds", verbose = FALSE)
 if(m$count()) m$drop()
+m2 <- mongo("test_diamonds_indices", verbose = FALSE)
 
 test_that("insert data", {
   m$insert(diamonds)
@@ -25,4 +26,64 @@ test_that("remove data", {
   m$remove('{}', just_one = FALSE)
   expect_equal(m$count(), 0L)
   expect_true(m$drop())
+})
+
+test_that("one can create unique index for single field", {
+  if (m2$count()) m2$drop()
+
+  diamonds2 <- diamonds[1:100, ]
+  diamonds2$simpleId <- 1:nrow(diamonds2)
+  resL <- m2$insert(diamonds2)
+  expect_equal(resL$nInserted, 100L)
+  expect_equal(list(), resL$writeErrors)
+  expect_equal(m2$count(),100L)
+
+  simpleIdJson <- toJSON(list("simpleId" = 1), auto_unbox = TRUE)
+  idxDf <- m2$index(add = simpleIdJson, addOpts = list(unique = TRUE))
+  expect_true(idxDf[idxDf$name == "simpleId_1", "unique"], info = "index created successfully")
+
+  expect_error(m2$insert(diamonds2[1,]), "E11000 duplicate key error collection")
+
+  myV <- c(rep(1,10),101)
+  names(myV) <- colnames(diamonds2)
+  resL <- m2$insert(data.frame(t(myV)))
+  expect_equal(resL$nInserted, 1L)
+  expect_equal(list(), resL$writeErrors)
+  expect_equal(m2$count(),101L)
+
+})
+
+test_that("one can create complex unique index", {
+  if (m2$count()) m2$drop()
+
+  diamonds2 <- diamonds[1:100, ]
+  diamonds2$simpleId <- 1:nrow(diamonds2)
+  diamonds3 <-
+    cbind(diamonds2,
+          secondId = rep(1:25, 4),
+          thirdId = rep(1:4, each = 25))
+  resL <- m2$insert(diamonds3)
+  expect_equal(resL$nInserted, 100L)
+  expect_equal(list(), resL$writeErrors)
+  expect_equal(m2$count(),100L)
+
+  complexIdxJson <- toJSON(list("secondId" = 1, "thirdId" = 1), auto_unbox = TRUE)
+  idxDf <- m2$index(add = complexIdxJson, addOpts = list(unique = TRUE))
+  expect_true(idxDf[idxDf$name == "secondId_1_thirdId_1", "unique"], info = "index created successfully")
+
+  expect_error(m2$insert(diamonds3[1,]), "E11000 duplicate key error collection")
+
+  myV <- c(rep(1, 10), c(100, 25, 4))
+  names(myV) <- colnames(diamonds3)
+  expect_error(
+    m2$insert(data.frame(t(myV))),
+    "E11000 duplicate key error collection.+secondId_1_thirdId_1 dup key"
+  )
+
+  myV <- c(rep(1, 10), c(100, 25, 5))
+  names(myV) <- colnames(diamonds3)
+  resL <- m2$insert(data.frame(t(myV)))
+  expect_equal(resL$nInserted, 1L)
+  expect_equal(list(), resL$writeErrors)
+  expect_equal(m2$count(),101L)
 })
