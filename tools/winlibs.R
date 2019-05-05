afd656540d292c96b899b41f9cf42db6698af987
Jeroen Ooms <jeroenooms@gmail.com>
2019-05-05 18:19:58 +0200

    Windows: update to openssl 1.1.1

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,8 +2,8 @@
 if(getRversion() < "3.3.0") setInternet2()
 
 # Download OpenSSL
-if(!file.exists("../windows/openssl-1.1.0f/include/openssl/ssl.h")){
-  download.file("https://github.com/rwinlib/openssl/archive/v1.1.0f.zip", "lib.zip", quiet = TRUE)
+if(!file.exists("../windows/openssl-1.1.1/include/openssl/ssl.h")){
+  download.file("https://github.com/rwinlib/openssl/archive/v1.1.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
