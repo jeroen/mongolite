20ff6a76e548248cbddab9a5bbbc9cd9dea35a94
Jeroen Ooms <jeroenooms@gmail.com>
2020-12-28 15:02:21 +0100

    Fix checks

--- R/hex.R
+++ R/hex.R
@@ -5,8 +5,11 @@
 #'
 #' @export
 #' @param string of length 24 containing the OID
+#' @param oid string or raw value with document oid
 #' @examples oid_to_timestamp('5349b4ddd2781d08c09890f3')
 oid_to_timestamp <- function(oid){
+  if(is.raw(oid))
+    oid <- paste0(oid, collapse = "")
   stopifnot("oid invalid" = nchar(oid) > 23)
   val <- strtoi(paste0('0x', substring(oid, 1, 8)))
   structure(val, class = c("POSIXct", "POSIXt"))
