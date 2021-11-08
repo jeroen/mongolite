41467c9916c41951c47d1502194e17c533cb8b08
Kevin Albertson <kevin.albertson@10gen.com>
2021-11-08 13:18:17 +0000

    Call mongoc_handshake_data_append (#225)
    * Call mongoc_handshake_data_append
    
    Prepend R runtime version to driver.platform.
    Append "mongolite" to driver.name.
    
    * Prefer HTTPUserAgent if available in handshake platform data

--- src/mongo_log.c
+++ src/mongo_log.c
@@ -1,4 +1,6 @@
+#include <Rinternals.h>
 #include <R_ext/Rdynload.h>
+#include <Rversion.h>
 #include <mongolite.h>
 
 //default
@@ -32,10 +34,25 @@ void mongolite_log_handler (mongoc_log_level_t event, const char *log_domain, co
 
 void R_init_mongolite(DllInfo *info) {
   static mongoc_log_func_t logfun = mongolite_log_handler;
+  char *r_version;
   mongoc_init();
+
+  SEXP agent = GetOption1(install("HTTPUserAgent"));
+  if (isString(agent) && Rf_length(agent)) {
+    r_version = bson_strdup_printf ("%s ", CHAR(STRING_ELT(agent, 0)));
+  }
+  else {
+#if defined (R_VERSION)
+  r_version = bson_strdup_printf ("R=%s.%s ", R_MAJOR, R_MINOR);
+#else
+  r_version = bson_strdup ("R=undefined ");
+#endif
+  }
+  mongoc_handshake_data_append ("mongolite", "", r_version);
   mongoc_log_set_handler(logfun, NULL);
   R_registerRoutines(info, NULL, NULL, NULL, NULL);
   R_useDynamicSymbols(info, TRUE);
+  bson_free (r_version);
 }
 
 void R_unload_mongolite(DllInfo *info) {
