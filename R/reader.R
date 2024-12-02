072d9465fbb1b1727a5ddcbc212d17307043a203
Jeroen Ooms <jeroenooms@gmail.com>
2024-12-02 21:49:24 +0100

    Use curl download because base R has terrible defaults for timeout and mode.

--- R/reader.R
+++ R/reader.R
@@ -2,8 +2,8 @@
 #'
 #' Utility to read BSON data into R without MongoDB. Useful to read a dump from
 #' from a `mongoexport` dump if it fits in memory. This utility does not attempt
-#' to convert data into a data.frame: the output is a vector with length equal
-#' to the number of documents in the collection.
+#' to convert data into a data.frame: the output is a vector with length of the
+#' total number of documents in the bson collection.
 #'
 #' Alternatively, to import a bson dump into your local mongodb server, use the
 #' [mongo$import][mongo] function instead. This requires little memory and once
@@ -11,14 +11,19 @@
 #'
 #' @export
 #' @useDynLib mongolite R_bson_reader_file
-#' @param file path to a bson file on disk
+#' @param file path or url to a bson file
 #' @param as_json read data into json strings instead of R lists.
 #' @param verbose print some progress output while reading
 #' @examples
-#' download.file("http://jeroen.github.io/data/diamonds.bson", "diamonds.bson")
-#' diamonds <- read_bson("diamonds.bson")
-#' unlink("diamonds.bson")
+#' diamonds <- read_bson("http://jeroen.github.io/data/diamonds.bson")
+#' length(diamonds)
 read_bson <- function(file, as_json = FALSE, verbose = interactive()){
+  if(grepl("^https?://", file)){
+    file_url <- file
+    file <- tempfile()
+    on.exit(unlink(file))
+    curl::curl_download(file_url, file, quiet = !isTRUE(verbose))
+  }
   file <- normalizePath(file, mustWork = TRUE)
   .Call(R_bson_reader_file, file, as_json, verbose)
 }
